bab95378fee757e893929fdd75429a142e96ded7
Ruzha Bobotanova <r.bobotanova@webfashion.bg>
2021-07-14 23:43:05 +0300

    create debug pipe

---
+++ src/app/shared/pipes/console-log.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'consoleLog'
+})
+export class ConsoleLogPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    console.log(`debug: ${value}`)
+    return null;
+  }
+
+}
